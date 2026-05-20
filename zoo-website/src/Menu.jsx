7771578939875b0cf35465811ea6d404a9f8b909
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-05-20 16:30:46 +0200

    Mache aus Menu ein BurgerMenu

--- zoo-website/src/Menu.jsx
+++ zoo-website/src/Menu.jsx
@@ -1,32 +1,35 @@
-import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react"
+import { HashRouter, Route, Routes, Link } from "react-router-dom"
 import "./Menu.css"
 
-export default function Menu(){
+export default function Menu() {
+    const [open, setOpen] = useState(false)
 
+    const closeMenu = () => setOpen(false)
 
-    return(
-        <>
-            <HashRouter>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home Page</Link>
-                            <Link to="/Tickets">Tickets bestellen</Link>
-                            <Link to="/News">Zoo News</Link>
-                            <Link to="/Impressum">Impressum</Link>
-                            <Link to="/Datenschutz">Datenschutz</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    {// <Route path="/" element={<HomePage />} />
-                        //<Route path="/Tickets" element={<Tickets />}
-                        //<Route path="/News" element={<News />}
-                        //<Route path="/Tickets" element={<Impressum />}
-                        //<Route path="/Datenschutz" element={<Datenschutz />}
-                    }
-                </Routes>
-            </HashRouter>
-        </>
+    return (
+        <HashRouter>
+            <nav className="nav">
+
+                <button
+                    className="burger"
+                    onClick={() => setOpen(!open)}
+                >
+                    ☰
+                </button>
+
+                <ul className={`menu ${open ? "open" : ""}`}>
+                    <li><Link to="/" onClick={closeMenu}>Home Page</Link></li>
+                    <li><Link to="/Tickets" onClick={closeMenu}>Tickets bestellen</Link></li>
+                    <li><Link to="/News" onClick={closeMenu}>Zoo News</Link></li>
+                    <li><Link to="/Impressum" onClick={closeMenu}>Impressum</Link></li>
+                    <li><Link to="/Datenschutz" onClick={closeMenu}>Datenschutz</Link></li>
+                </ul>
+            </nav>
+
+            <Routes>
+                {/* Routes kommen hier rein */}
+            </Routes>
+        </HashRouter>
     )
 }
