5e54295b1f4c0f308dee39b5b4ff6e27628cdbdc
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-05-20 15:14:44 +0200

    refactor: transfer App.jsx code into Home.jsx

--- zoo-website/src/App.jsx
+++ zoo-website/src/App.jsx
@@ -3,20 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from "./components/Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="welcome_body">
-        <span className="welcome_text">
-          Zubi schegani
-        </span>
-        <img className="welcome_image" src={heroImg} />
-
-      </div>
-    </>
+    <Home/>
   )
 }
 
---
+++ zoo-website/src/components/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+
+    return (
+        <div className="welcome_body">
+        <span className="welcome_text">
+          Zubi schegani
+        </span>
+            <img className="welcome_image" src={heroImg}/>
+
+        </div>
+    )
+}
+
