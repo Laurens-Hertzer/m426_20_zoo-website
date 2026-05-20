31ff8eec0de6eb49d7c342ba4bb3d97116a657f0
Timure228 <arductimur@gmail.com>
2026-05-20 15:28:58 +0200

    fix: image import fix home page

--- zoo-website/src/components/Home.jsx
+++ zoo-website/src/components/Home.jsx
@@ -1,12 +1,12 @@
-export default function Home() {
+import welcome_img from '../assets/welcome_img.jpg'
 
+export default function Home() {
     return (
         <div className="welcome_body">
         <span className="welcome_text">
-          Zubi schegani
+          Josua's beschreibung Me te se te pe te fe te ce le me ne so to ppe to
         </span>
-            <img className="welcome_image" src={heroImg}/>
-
+            <img className="welcome_image" src={welcome_img} width={600} height={400}/>
         </div>
     )
 }
