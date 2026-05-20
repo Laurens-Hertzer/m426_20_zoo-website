d2114a661ff02f6c61e0b47915c5478c20e0bcac
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-05-20 15:35:09 +0200

    feat: added Impressum with details to the zoo and us

--- zoo-website/src/App.jsx
+++ zoo-website/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import Home from "./components/Home.jsx";
+import Impressum from "./components/Impressum.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <Home/>
-  )
+    return (
+        <div>
+            <Home/>
+            <Impressum/>
+        </div>
+    )
 }
 
 export default App
