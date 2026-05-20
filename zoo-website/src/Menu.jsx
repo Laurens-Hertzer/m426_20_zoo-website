af299a5f479a6aae7bd74dd7efcbc8f6c13908e0
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-05-20 15:53:03 +0200

    Erstelle Vorlage

--- zoo-website/src/Menu.jsx
+++ zoo-website/src/Menu.jsx
@@ -1,10 +1,31 @@
-
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
 
 export default function Menu(){
 
 
     return(
         <>
+            <HashRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home Page</Link>
+                            <Link to="/Tickets">Tickets bestellen</Link>
+                            <Link to="/News">Zoo News</Link>
+                            <Link to="/Impressum">Impressum</Link>
+                            <Link to="/Datenschutz">Datenschutz</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    {// <Route path="/" element={<HomePage />} />
+                        //<Route path="/Tickets" element={<Tickets />}
+                        //<Route path="/News" element={<News />}
+                        //<Route path="/Tickets" element={<Impressum />}
+                        //<Route path="/Datenschutz" element={<Datenschutz />}
+                    }
+                </Routes>
+            </HashRouter>
         </>
     )
 }
