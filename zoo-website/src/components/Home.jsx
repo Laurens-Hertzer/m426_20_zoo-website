2eb483efd70ca59d6da826ff09463c9b9dd1bb12
Timure228 <arductimur@gmail.com>
2026-05-20 15:59:00 +0200

    feat: add text and borders to the home page

--- zoo-website/src/components/Home.jsx
+++ zoo-website/src/components/Home.jsx
@@ -2,12 +2,19 @@ import welcome_img from '../assets/welcome_img.jpg'
 
 export default function Home() {
     return (
-        <div className="welcome_body">
-        <span className="welcome_text">
-          Josua's beschreibung Me te se te pe te fe te ce le me ne so to ppe to
-        </span>
-            <img className="welcome_image" src={welcome_img} width={600} height={400}/>
-        </div>
+        <>
+            <div className="welcome_body">
+                <span className="welcome_text"> <h2>Willkommen zum Zoo Zürich</h2>
+                Tauche ein in die faszinierende Welt der Tiere.
+                Mitten in Zürich erwarten dich über 300 Tierarten in naturnah gestalteten Lebensräumen. Erlebe die asiatischen Elefanten im Kaeng Krachan Elefantenpark, beobachte majestätische Löwen in der Lewa Savanne oder spaziere durch den tropischen Masoala Regenwald.
+                Ob Familienausflug, Abenteuer mit Freunden oder ein entspannter Spaziergang – der Zoo Zürich bietet unvergessliche Momente für Gross und Klein. Wir engagieren uns leidenschaftlich für den Natur- und Artenschutz und laden dich ein, Teil dieser Mission zu sein.
+                Wilkommen im Zoo Zürich
+                Tauche ein in die faszinierende Welt der Tiere.
+                Mitten in Zürich erwarten dich über 300 Tierarten in naturnah gestalteten Lebensräumen. Erlebe die asiatischen Elefanten im Kaeng Krachan Elefantenpark, beobachte majestätische Löwen in der Lewa Savanne oder spaziere durch den tropischen Masoala Regenwald.
+                Ob Familienausflug, Abenteuer mit Freunden oder ein entspannter Spaziergang – der Zoo Zürich bietet unvergessliche Momente für Gross und Klein. Wir engagieren uns leidenschaftlich für den Natur- und Artenschutz und laden dich ein, Teil dieser Mission zu sein.
+                </span>
+                <img className="welcome_image" src={welcome_img} width={600} height={400}/>
+            </div>
+        </>
     )
 }
-
