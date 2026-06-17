8d3215c3e21b94c18dab2c45e1070b37887fa552
JosuaWelsche <josua.welsche@stud.kbw.ch>
2026-06-17 12:21:50 +0200

    fix: layout structure

--- zoo-website/src/App.jsx
+++ zoo-website/src/App.jsx
@@ -1,6 +1,4 @@
-
-import websitelogo from './assets/favicon.png'
-import './App.css'
+import './css/App.css'
 import Menu from './components/Menu'
 import '../src/css/zoo-zuerich-design.css'
 
@@ -8,9 +6,7 @@ function App() {
 
     return (
         <>
-            <header>
-                <Menu/>
-            </header>
+            <Menu/>
         </>
     )
 }
--- zoo-website/src/components/Menu.jsx
+++ zoo-website/src/components/Menu.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { HashRouter, Route, Routes, Link } from "react-router-dom"
+import React, {useState} from "react"
+import {HashRouter, Route, Routes, Link} from "react-router-dom"
 import ZooImg from "../assets/favicon.png"
 import DataProtection from "./DataProtection.jsx"
 import Impressum from "./Impressum.jsx"
@@ -8,31 +8,37 @@ import News from "./News.jsx";
 import NewsStoryDetail from "./NewsStoryDetail.jsx";
 
 export default function Menu() {
+
     const [open, setOpen] = useState(false)
     const closeMenu = () => setOpen(false)
 
     return (
         <HashRouter>
-            <Link to="/"><img src={ZooImg} alt="Zoo"/></Link>
-            <nav className="nav">
-                <button className="burger" onClick={() => setOpen(!open)}>☰</button>
-                <ul className={`menu ${open ? "open" : ""}`}>
-                    <li><Link to="/" onClick={closeMenu}>Home Page</Link></li>
-                    <li><Link to="/Tickets" onClick={closeMenu}>Tickets bestellen</Link></li>
-                    <li><Link to="/News" onClick={closeMenu}>News</Link></li>
-                    <li><Link to="/Impressum" onClick={closeMenu}>Impressum</Link></li>
-                    <li><Link to="/Datenschutz" onClick={closeMenu}>Datenschutz</Link></li>
-                </ul>
-            </nav>
-
-            <Routes>
-                {/* Routes kommen hier rein */}
-                <Route path="/Datenschutz" element={<DataProtection />} />
-                <Route path="/Impressum" element={<Impressum />} />
-                <Route path="/Tickets" element={<Tickets />} />
-                <Route path="/News" element={<News />} />
-                <Route path="/News/:storyId" element={<NewsStoryDetail />} />
-            </Routes>
+            <header className="site-header">
+                <Link to="/" className="logo-link">
+                    <img src={ZooImg} alt="Zoo"/>
+                </Link>
+                <nav className="nav">
+                    <button className="burger" onClick={() => setOpen(!open)}>☰</button>
+                    <ul className={`menu ${open ? "open" : ""}`}>
+                        <li><Link to="/" onClick={closeMenu}>Home Page</Link></li>
+                        <li><Link to="/Tickets" onClick={closeMenu}>Tickets bestellen</Link></li>
+                        <li><Link to="/News" onClick={closeMenu}>News</Link></li>
+                        <li><Link to="/Impressum" onClick={closeMenu}>Impressum</Link></li>
+                        <li><Link to="/Datenschutz" onClick={closeMenu}>Datenschutz</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Routes>
+                    {/* Routes kommen hier rein */}
+                    <Route path="/Datenschutz" element={<DataProtection/>}/>
+                    <Route path="/Impressum" element={<Impressum/>}/>
+                    <Route path="/Tickets" element={<Tickets/>}/>
+                    <Route path="/News" element={<News/>}/>
+                    <Route path="/News/:storyId" element={<NewsStoryDetail/>}/>
+                </Routes>
+            </main>
         </HashRouter>
     )
 }
