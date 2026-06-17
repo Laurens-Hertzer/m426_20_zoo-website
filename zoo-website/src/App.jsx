2c961e8ea7f74b10e83346ce1c1a9043f25c9284
Timure228 <arductimur@gmail.com>
2026-06-17 16:45:48 +0200

    feat: basic shop layout added with few souvenirs, image, price and a buy button

--- zoo-website/src/App.jsx
+++ zoo-website/src/App.jsx
@@ -1,8 +1,12 @@
-
+import {useState} from 'react'
 import websitelogo from './assets/favicon.png'
 import './App.css'
-import Menu from './components/Menu'
 import '../src/css/zoo-zuerich-design.css'
+import Menu from './components/Menu.jsx'
+import Home from "./components/Home.jsx";
+import Tickets from "./components/Tickets.jsx";
+import Impressum from "./components/Impressum.jsx";
+import Shop from "./components/Shop.jsx";
 
 function App() {
 
@@ -11,6 +15,12 @@ function App() {
             <header>
                 <Menu/>
             </header>
+            <div>
+                <Home/>
+                <Tickets/>
+                <Shop/>
+                <Impressum/>
+            </div>
         </>
     )
 }
